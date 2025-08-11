ee58965477eee41dab1a7b464ef94bf010dec724
HarryHoFF <caseygs1984@gmail.com>
2025-08-11 21:07:44 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ export default function App() {
 
 const transactions: Transaction[] = [
   // Mai 2025 
+  { id: 208, name: "Kerstin Laabs", date: "11/08/2025", amount: "75,80 €", type: "income" },
   { id: 207, name: "Josy Hohberg", date: "10/08/2025", amount: "4,50 €", type: "income" },
   { id: 206, name: "Josy Hohberg", date: "10/08/2025", amount: "21,50 €", type: "income" },
   { id: 205, name: "Deutsche Post AG", date: "08/08/2025", amount: "-12,38 €", type: "outcome" },
@@ -224,7 +225,7 @@ const transactions: Transaction[] = [
 
       <div className="balance-card">
         <p>Din saldo</p>
-        <h2>4.835,41 €</h2>
+        <h2>4.916,21 €</h2>
       </div>
 
       <div className="button-container">
